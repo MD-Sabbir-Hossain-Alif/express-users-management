90aff817bc7a1c209e3592ab3ee93af3166670b0
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2026-05-06 00:52:54 +0600

    add post method

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8000
+const cors = require('cors')
+
+// midddleware
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello from users World!')
@@ -8,12 +13,24 @@ app.get('/', (req, res) => {
 
 const users = [
     { id: 1, name: 'sabbir', email: "sabbir@example" },
-    { id: 1, name: 'abbir', email: "abbir@example" },
-    { id: 1, name: 'saba', email: "saba@example" }
+    { id: 2, name: 'abbir', email: "abbir@example" },
+    { id: 3, name: 'saba', email: "saba@example" }
 ]
+
+console.log(users)
 
 app.get('/users', (req, res) => {
     res.send(users)
+})
+
+app.post('/users', (req, res) => {
+    console.log("data in the request", req.body)
+
+    const newUser = req.body
+    newUser.id = users.length + 1
+    users.push(newUser)
+
+    res.send({ success: true, data: newUser, message: "post method is working" })
 })
 
 app.get('/products', (req, res) => {
